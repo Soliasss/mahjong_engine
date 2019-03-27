3622c55fa156e946873b082b57586abfbc2517fa
Qwiran <ant.bzh@outlook.fr>
2019-03-27 15:01:50 +0100

    Add javadoc and update of PlayerSet and PlayerSituation

--- engine/src/main/java/fr/univubs/inf1603/mahjong/engine/PlayerSet.java
+++ engine/src/main/java/fr/univubs/inf1603/mahjong/engine/PlayerSet.java
@@ -36,13 +36,85 @@ public class PlayerSet {
     public PlayerSet(GameTile winningTile, Collection<Combination> hand, Collection<Combination> concealed, Collection<Combination> melds, Collection<SupremeHonor> supremeHonors,
             boolean drawnFromWall, boolean takenFromDiscard, WindHonor.Wind roundWind, WindHonor.Wind playerWind){
         this.winningTile = winningTile;
-        this.hand = new ArrayList<Combination>();
-        this.concealed = new ArrayList<Combination>();
-        this.melds = new ArrayList<Combination>();
-        this.supremeHonors = new ArrayList<SupremeHonor>();
+        this.hand = new ArrayList<Combination>(hand);
+        this.concealed = new ArrayList<Combination>(concealed);
+        this.melds = new ArrayList<Combination>(melds);
+        this.supremeHonors = new ArrayList<SupremeHonor>(supremeHonors);
         this.drawnFromWall = drawnFromWall;
         this.takenFromDiscard = takenFromDiscard;
         this.roundWind = roundWind;
         this.playerWind = playerWind;
+    }
+    
+    /**
+     *
+     * @return winningTile The last tile drawn or stolen which allow us to win 
+     */
+    public GameTile getWinningTile(){
+        return this.winningTile;
+    }
+    
+    /**
+     *
+     * @return hand It's a list of combination in the player's hand
+     */
+    public Collection<Combination> getHand(){
+        return this.hand;
+    }
+    
+    /**
+     *
+     * @return concealed It's a list of combination if some tiles are hidden
+     */
+    public Collection<Combination> getConcealed(){
+        return this.concealed;
+    }
+    
+    /**
+     *
+     * @return melds It's a list of combination about tiles set down
+     */
+    public Collection<Combination> getMelds(){
+        return this.melds;
+    }
+    
+    /**
+     *
+     * @return It's a list of supremeHonors set down
+     */
+    public Collection<SupremeHonor> getSupremeHonors(){
+        return this.supremeHonors;
+    }
+    
+    /**
+     *
+     * @return true or false if the last tile was drawn from the wall
+     */
+    public boolean isDrawnForWall(){
+        return this.drawnFromWall;
+    }
+    
+    /**
+     *
+     * @return true or false if the last tile was stolen
+     */
+    public boolean isTakenFromDiscard(){
+        return this.takenFromDiscard;
+    }
+    
+    /**
+     *
+     * @return the wind during this round if his a upward or downward wind
+     */
+    public WindHonor.Wind getRoundWind(){
+        return this.roundWind;
+    }
+    
+    /**
+     *
+     * @return the wind of the player, it could be east, south, weast or north
+     */
+    public WindHonor.Wind getPlayerWind(){
+        return this.playerWind;
     }
 }
