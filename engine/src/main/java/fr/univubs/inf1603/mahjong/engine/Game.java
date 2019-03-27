3b056f548fc497f43c9d306748be9d36bac2b402
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2019-03-27 16:23:38 +0100

    Update Game.java

--- engine/src/main/java/fr/univubs/inf1603/mahjong/engine/Game.java
+++ engine/src/main/java/fr/univubs/inf1603/mahjong/engine/Game.java
@@ -5,5 +5,23 @@ package fr.univubs.inf1603.mahjong.engine;
  * @author COGOLUEGNES Charles
  */
 public class Game{
-
+  private HashMap<Integer,Wind> listPlayers;
+  private boolean isOver;
+  private Move lastMove;
+  
+  /**
+   * Le constructeur de Game
+   * @param rule La règle qui va être jouée
+   */
+  public Game(GameRule rule){
+    
+  }
+  
+  /**
+   * Permet de lancer la partie
+   * @return une map faisant correspondance entre le numéro du joueur et le vent
+   */
+  public Hash<Integer,Wind> launchGame(){
+    
+  }
 }
