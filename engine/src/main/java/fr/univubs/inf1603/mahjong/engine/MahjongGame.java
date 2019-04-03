6bdeb84fae1f30be556b051bc88b0e633fed265b
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2019-04-03 15:10:34 +0200

    Update MahjongGame.java

--- engine/src/main/java/fr/univubs/inf1603/mahjong/engine/MahjongGame.java
+++ engine/src/main/java/fr/univubs/inf1603/mahjong/engine/MahjongGame.java
@@ -108,4 +108,22 @@ public class MahjongGame implements Persistable{
   public Game clone(){
     return null;
   }
+  
+  private final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+  public void addPropertyChangeListener(PropertyChangeListener listener) {
+      propertyChangeSupport.addPropertyChangeListener(listener);
+  }
+
+  public void addPropertyChangeListener(String propertyName, PropertyChangeListener listener) {
+      propertyChangeSupport.addPropertyChangeListener(propertyName, listener);
+  }
+
+  public void removePropertyChangeListener(PropertyChangeListener listener) {
+      propertyChangeSupport.removePropertyChangeListener(listener);
+  }
+
+  public void removePropertyChangeListener(String propertyName, PropertyChangeListener listener) {
+      propertyChangeSupport.removePropertyChangeListener(propertyName, listener);
+  }
 }
