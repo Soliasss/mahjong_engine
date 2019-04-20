e4f954d3b1d9c4d516e6462820367ec693086511
8000ff <clementmallejac@gmail.com>
2019-04-20 20:18:42 +0200

    Added interface AsbtractGameRuleFactory

---
+++ engine/src/main/java/fr/univubs/inf1603/mahjong/engine/rule/AbstractGameRuleFactory.java
@@ -0,0 +1,24 @@
+package fr.univubs.inf1603.mahjong.engine.rule;
+
+/**
+ * This interface represent the ability of its implementations to create or to give a reference of a GameRule
+ * @author purpl
+ */
+public interface AbstractGameRuleFactory {
+    /**
+     * Creates or searches for a GameRule based on a given name
+     * @param name Provided name
+     * @return A GameRule found or created based on this provided name
+     * @throws RulesException If the factory is unable to find or create a GameRule based on this provided name
+     */
+    public GameRule create(String name) throws RulesException;
+    
+    /**
+     * Creates or searches for a GameRule based on a BoardRule and a ScoringSystem
+     * @param boardRule 
+     * @param scoringSystem
+     * @return A GameRule 
+     * @throws RulesException 
+     */
+    public GameRule create(BoardRule boardRule, ScoringSystem scoringSystem) throws RulesException;
+}
