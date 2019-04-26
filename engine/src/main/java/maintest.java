a17b26ee302e3b2181836a8a64050129a3978f4f
8000ff <clementmallejac@gmail.com>
2019-04-26 21:48:47 +0200

    Cahnged calls to Move's constructor to match new signature

---
+++ engine/src/main/java/maintest.java
@@ -0,0 +1,45 @@
+
+import fr.univubs.inf1603.mahjong.engine.game.GameException;
+import fr.univubs.inf1603.mahjong.engine.game.MahjongBoard;
+import fr.univubs.inf1603.mahjong.engine.game.MahjongGame;
+import fr.univubs.inf1603.mahjong.engine.game.Move;
+import fr.univubs.inf1603.mahjong.engine.rule.GameRule;
+import fr.univubs.inf1603.mahjong.engine.rule.GameRuleFactory;
+import fr.univubs.inf1603.mahjong.engine.rule.RulesException;
+import fr.univubs.inf1603.mahjong.engine.rule.Wind;
+import java.time.Duration;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author purpl
+ */
+public class maintest {
+    public static void main(String[] args) throws GameException{
+        GameRule rule = null;
+        try {
+            rule = new GameRuleFactory().create("SILLY_RULE");
+        } catch (RulesException ex) {
+            System.exit(0);
+        }
+        
+        
+        MahjongBoard board = new MahjongBoard(Wind.WEST);
+        
+        
+        MahjongGame game;
+        game = new MahjongGame(rule, board, (Move)null, Duration.ofSeconds(5), Duration.ofSeconds(5), new int[4], UUID.randomUUID(), Wind.values());
+        System.out.println("Passed construction");
+        
+        game.launchGame();
+        
+    }
+}
