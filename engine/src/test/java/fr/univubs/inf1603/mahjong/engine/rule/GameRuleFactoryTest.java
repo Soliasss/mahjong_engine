c8dc059b4300f4e2bfb4dbc7aacb0839bb54af55
8000ff <39583821+8000ff@users.noreply.github.com>
2019-04-30 09:47:21 +0200

    Missing Wind import

--- engine/src/test/java/fr/univubs/inf1603/mahjong/engine/rule/GameRuleFactoryTest.java
+++ engine/src/test/java/fr/univubs/inf1603/mahjong/engine/rule/GameRuleFactoryTest.java
@@ -5,6 +5,8 @@
  */
 package fr.univubs.inf1603.mahjong.engine.rule;
 
+import fr.univubs.inf1603.mahjong.Wind;
+
 import fr.univubs.inf1603.mahjong.engine.game.MahjongBoard;
 import fr.univubs.inf1603.mahjong.engine.game.Move;
 import fr.univubs.inf1603.mahjong.engine.rule.GameRuleList.Rule;
